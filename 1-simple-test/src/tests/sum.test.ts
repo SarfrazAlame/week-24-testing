d5184e1657f87dc1700f7706ab14ded11f692682
Sarfraz Alam <sarfrazk7858@gmail.com>
2025-05-08 06:20:56 +0530

    simple text done

--- 1-simple-test/src/tests/sum.test.ts
+++ 1-simple-test/src/tests/sum.test.ts
@@ -1,8 +1,19 @@
-import {describe, expect, test} from '@jest/globals';
-import {sum} from '../index';
+import {describe, expect, it, test} from '@jest/globals';
+import {multiply, sum} from '../index';
 
 describe('sum module', () => {
-  test('adds 1 + 2 to equal 3', () => {
+  it('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
+  it('should return the sum of negative numbers correctly', ()=>{
+    const finalAnswer = sum(-1,-2);
+    expect(finalAnswer).toBe(-3);
+  })
 });
+
+describe('multiply number', ()=>{
+  it('multiply 1 and 2 to equal 2', ()=>{
+    const finalAnswer = multiply(1,2);
+    expect(finalAnswer).toBe(2)
+  })
+})
