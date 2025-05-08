d30d3076544e159000b805fe263190a1a6ac7da5
Sarfraz Alam <sarfrazk7858@gmail.com>
2025-05-08 10:05:05 +0530

    testing

--- 2-simple-express-app/src/tests/sum.test.ts
+++ 2-simple-express-app/src/tests/sum.test.ts
@@ -1,32 +1,25 @@
-import {describe, expect, test, it} from '@jest/globals';
-import request from "supertest";
+import {describe, expect, it} from "@jest/globals"
+import request from "supertest"
 import { app } from "../index"
 
-describe("POST /sum", () => {
-    it("should return the sum of two numbers", async () => {
-        const res = await request(app).post("/sum").send({
-          a: 1,
-          b: 2
-        });
-        expect(res.statusCode).toBe(200);
-        expect(res.body.answer).toBe(3);
-      });
 
-      it("should return the sum of two negative numbers", async () => {
-        const res = await request(app).post("/sum").send({
-          a: -1,
-          b: -2
-        });
-        expect(res.statusCode).toBe(200);
-        expect(res.body.answer).toBe(-3);
-      });
+describe("POST /sum", ()=>{
+  it('it should return the sum of two number', async()=>{
+    const res = await request(app).post('/sum').send({
+      a:1,
+      b:2
+    })
 
-      it("should return the sum of two zero number", async () => {
-        const res = await request(app).post("/sum").send({
-          a: 0,
-          b: 0
-        });
-        expect(res.statusCode).toBe(200);
-        expect(res.body.answer).toBe(0);
-      });
-});
+    expect(res.statusCode).toBe(200)
+    expect(res.body.answer).toBe(3)
+  })
+  it('should return the sum of two negative number', async()=>{
+    const res = await request(app).post('/sum').send({
+      a:-1,
+      b:-2
+    })
+    expect(res.statusCode).toBe(200)
+    expect(res.body.answer).toBe(-3)
+  })
+})
+
